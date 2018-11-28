9c5162c76aeebe350590f600a0eed52faf0f15b2
David <davidkueng@gmail.com>
2018-11-28 20:43:56 +0100

    version with wav files

--- drumMachine.js
+++ drumMachine.js
@@ -1,6 +1,4 @@
-import { sampleFunction, Kick } from './drumSamples';
-
-let context = new AudioContext
+// let context = new AudioContext
 
 // class Kick {
 //     constructor(context) {
@@ -23,7 +21,36 @@ let context = new AudioContext
 //     }
 // };
 
-sampleFunction();
+// class Snare {
+//     constructor(context) {
+//         this.context = context;
+//     }
+//     setup() {
+//         this.osc = this.context.createOscillator();
+//         this.gain = this.context.createGain();
+//         this.osc.connect(this.gain);
+//         this.gain.connect(this.context.destination);
+//     }
+//     trigger(time) {
+//         this.setup();
+//         this.osc.frequency.setValueAtTime(75, time);
+//         this.gain.gain.setValueAtTime(1, time);
+//         this.osc.frequency.exponentialRampToValueAtTime(0.1, time + 0.5);
+//         this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
+//         this.osc.start(time);
+//         this.osc.stop(time + 0.5);
+//     }
+// };
 
-var kick = new Kick(context);
-kick.trigger(0);
+
+
+// let kick = new Kick(context);
+// // kick.trigger(0);
+
+// let snare = new Snare(context);
+// snare.trigger(0.5)
+
+
+document.addEventListener('click', () => {
+    document.querySelector('#Q').play();
+})
