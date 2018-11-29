1fce024f304ae16c7ef712030ef2a0d0647c98d3
David <davidkueng@gmail.com>
2018-11-29 20:58:36 +0100

    alibi commit

--- drumMachine.js
+++ drumMachine.js
@@ -1,56 +1,22 @@
-// let context = new AudioContext
+// document.addEventListener('click', (e) => {
+//     document.querySelector('#X').play();
+// })
 
-// class Kick {
-//     constructor(context) {
-//         this.context = context;
-//     }
-//     setup() {
-//         this.osc = this.context.createOscillator();
-//         this.gain = this.context.createGain();
-//         this.osc.connect(this.gain);
-//         this.gain.connect(this.context.destination);
-//     }
-//     trigger(time) {
-//         this.setup();
-//         this.osc.frequency.setValueAtTime(150, time);
-//         this.gain.gain.setValueAtTime(1, time);
-//         this.osc.frequency.exponentialRampToValueAtTime(0.1, time + 0.5);
-//         this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
-//         this.osc.start(time);
-//         this.osc.stop(time + 0.5);
-//     }
-// };
+// document.addEventListener('click', (e) => {
+//     document.querySelector('#W').play();
+// })
 
-// class Snare {
-//     constructor(context) {
-//         this.context = context;
-//     }
-//     setup() {
-//         this.osc = this.context.createOscillator();
-//         this.gain = this.context.createGain();
-//         this.osc.connect(this.gain);
-//         this.gain.connect(this.context.destination);
-//     }
-//     trigger(time) {
-//         this.setup();
-//         this.osc.frequency.setValueAtTime(75, time);
-//         this.gain.gain.setValueAtTime(1, time);
-//         this.osc.frequency.exponentialRampToValueAtTime(0.1, time + 0.5);
-//         this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
-//         this.osc.start(time);
-//         this.osc.stop(time + 0.5);
-//     }
-// };
+// let value = document.getElementById("test").value;
 
+// console.log(value)
 
+// .document.addEventListener('click', () => {
+//     document.querySelector("#Q").play();
+// })
 
-// let kick = new Kick(context);
-// // kick.trigger(0);
-
-// let snare = new Snare(context);
-// snare.trigger(0.5)
-
-
-document.addEventListener('click', () => {
-    document.querySelector('#Q').play();
-})
+let playSound = () => {
+    // document.addEventListener(() => {
+        document.querySelector("#Q").play();
+        document.querySelector("#W").play();
+    // })
+}
