488944b1e9f54a1b78aa4221cb5117eb61c9d474
David <davidkueng@gmail.com>
2018-11-30 22:45:50 +0100

    not getting anywhere

--- drumMachine.js
+++ drumMachine.js
@@ -1,12 +1,31 @@
 
 
 // let playSound = (e) => {
-//     // document.addEventListener(() => {
-//         let id = e.id
-//         console.log(id)
-//         document.querySelector('#W').play();
-//         // document.querySelector("#W").play();
-//     // })
+//     const button = document.getElementById(`"#${e.id}"`)
+//     button.addEventListener('click', () => {
+//         // let id = e.id
+//         // console.log(id)
+//         // document.querySelector(`${e.id}`).play();
+//         document.querySelector("#W").play();
+//     })
+
 // }
 
 
+// document.addEventListener("keydown", (e) => {
+    // console.log(`first keydown event. key property value is "${e.key}"`)
+
+    // let key = `"#${e.key}"`;
+    // console.log(key)
+
+    // document.querySelector(key).play()
+
+    // if (event.key === document.getElementById(event.key)) {
+        // document.querySelector(event.key).play()
+    // }
+
+    // console.log(event.key)
+// })
+
+
+console.log(document.getElementsByTagNameNS("DIV")[0].id)
